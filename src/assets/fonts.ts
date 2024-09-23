dbc47bb5fbc5fac358f75bba7a925a18d066e754
Kaushal Meena <kaushal.meena1996@gmail.com>
2024-09-23 16:58:57 +0530

    fix: resolve fonts folder

--- src/assets/fonts.ts
+++ src/assets/fonts.ts
@@ -1,9 +1,12 @@
-import { readFileSync } from "fs";
+import { readFileSync } from "node:fs";
+import { resolve } from "node:path";
+
+const FONT_FOLDER = resolve("public", "fonts");
 
 export const playpenSans400FontData = readFileSync(
-  `./public/fonts/PlaypenSans-400.ttf`
+  `${FONT_FOLDER}/PlaypenSans-400.ttf`
 );
 
 export const playpenSans500FontData = readFileSync(
-  `./public/fonts/PlaypenSans-500.ttf`
+  `${FONT_FOLDER}/PlaypenSans-500.ttf`
 );
